f709e0035c4315eb670dfee17f776d33a94a418e
alish9877 <alshaikhali302@gmail.com>
2025-01-13 14:47:03 +0300

    first

---
+++ collector/main_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Define the home view
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    # return HttpResponse("<h1>About the cat collector</h1>")
+    return render(request, 'about.html')
